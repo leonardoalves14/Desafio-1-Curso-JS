e7924bdbac1b96e06bf7f699265ee85e23f087dc
LeonardoAlves <leeoalvees_14@live.com>
2023-02-21 17:13:04 -0300

    Primeiro desafio do Curso de JavaScript

---
+++ index.js
@@ -0,0 +1,6 @@
+
+let favoriteFood = 'Lasanha'
+document.getElementById('text').innerHTML = favoriteFood
+
+let addJs = 'E essa linha sendo adicionada inteiramente com JS'
+document.getElementById('add').innerHTML = addJs
